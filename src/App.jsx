769f0da67b380a0eeb55365bac489f29e4518af7
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-10 08:23:48 -0400

    import react dom, TooltipProvider, AppToaster and Sonner

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import { Toaster as AppToaster } from "./components/ui/toaster";
+import { Toaster as SonnerToaster } from "./components/ui/sonner";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <AppToaster />
+      <SonnerToaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
