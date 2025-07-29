ce4b9ddc8e4107900dc926d4655e406c6ede5eed
bhogesh4i9 <vurabhogesh@gmail.com>
2025-07-29 03:11:12 -0400

    Toaster was imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
-import { Toaster} from "./components/ui/toaster";
 import { Toaster as SonnerToaster } from "./components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import { Toaster } from "./components/ui/Toaster";
+
 
 const queryClient = new QueryClient();
 
